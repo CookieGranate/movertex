cc8e414f8e63c3327e410d2c1add4cf6f6377a35
CookieGranate <cookiesooslol@gmail.com>
2024-12-26 22:21:30 +0100

    Navbar fertig

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,24 +7,25 @@ export default function Navbar() {
         <nav className="p-4 flex justify-between items-center w-full backdrop-blur bg-white bg-opacity-30 border-b border-gray-500">
             <ul className="flex space-x-4 flex-grow-0">
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="flex items-center space-x-4">
                             <Image alt="Icon" width={50} height={50} src={"/img/Movertex_icon.png"} />
+                            <p className="text-lg font-semibold">MOVertex</p>
                     </Link>
                 </li>
             </ul>
             <ul className="flex space-x-4 justify-center flex-grow">
                 <li>
-                    <Link href="/">
+                    <Link href="/" className="font-bold text-lg">
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link href="/portfolio">
+                    <Link href="/portfolio" className="font-bold text-lg">
                         Portfolio
                     </Link>
                 </li>
                 <li>
-                    <Link href="/contact">
+                    <Link href="/contact" className="font-bold text-lg">
                         Contact
                     </Link>
                 </li>
